5e017f08cc2ec89c246574fc48a99ec4401e8c20
sykes836 <bernard.sykes836@gmail.com>
2018-08-12 14:19:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+ function distanceFromHqInBlocks(dest) {
+   return Math.abs(42 - dest)
+ }
+
+ function distanceFromHqInFeet(dest) {
+   return distanceFromHqInBlocks(dest) * 264
+ }
+
+ function distanceTravelledInFeet(start, dest) {
+   return Math.abs(start-dest) * 264
+ }
+
+ function calculatesFarePrice(start, destination) {
+   const dist = Math.abs(start - destination) * 264
+
+   if (dist < 400) {
+     return 0
+   } else if (dist < 2000) {
+     return dist * .02
+   } else if (dist < 2500) {
+     return 25
+   } else {
+     return 'cannot travel that far'
+   }
+ }
