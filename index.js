a77c9067693191542fa4d1f8d84de39ed353b44a
sykes836 <bernard.sykes836@gmail.com>
2018-08-12 14:24:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@
 
    if (dist < 400) {
      return 0
-   } else if (dist < 2000) {
+   } else if (dist > 400 && dist < 2000) {
      return dist * .02
    } else if (dist < 2500) {
      return 25
